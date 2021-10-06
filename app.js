38e18922f66861fd33052acda5c1b2171c2512c3
BenKwon <kwon5891@gmail.com>
2021-10-06 15:40:00 +0900

    Complete verifyToken Router

--- app.js
+++ app.js
@@ -36,12 +36,15 @@ app.use(morgan('combined'));
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user-router");
 const loginLogRouter = require("./routes/loginLog-router");
+const verifyTokenRouter = require("./routes/verifyToken");
 app.get("/", (req,res)=>{
     res.send("success");
 })
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
 app.use("/loginLog", loginLogRouter);
+app.use("/verifyToken", verifyTokenRouter);
+
 app.listen(app.get("port"), () => {
     console.log(app.get("port"), "번 포트에서 대기 중");
 });
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -4,7 +4,35 @@ const router = express.Router();
 
 // router.get("/:id", UserController.getUser);
 router.post("/", (req,res,next)=>{
-
+    const authHeader = req.headers.token;
+    console.log(authHeader);
+    if (authHeader) {
+        //type Bearer
+        const token = authHeader.split(" ")[1];
+        console.log("token", token);
+        let result
+        jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+            if (err) {
+                result ={
+                    "result" : "fail",
+                    "pass" : false,
+                    "msg" : "Token is not valid"
+                }
+                res.status(403).json(result);
+            }
+            else {
+                req.user = user;
+                result = {
+                    "result" : "success",
+                    "msg" : "Token is valid",
+                    "pass" : true
+                }
+                res.status(201).json(result);
+            }
+        });
+    } else {
+        return res.status(401).json("You are not authenticated");
+    }
 });
 
 module.exports = router;

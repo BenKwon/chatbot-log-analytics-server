a294a44ced059d273a13281c747ce7e0a3d07767
BenKwon <kwon5891@gmail.com>
2021-10-15 15:08:36 +0900

    Update add router, controller, service about top5 intents

--- routes/sampling-router.js
+++ routes/sampling-router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const SamplingController = require("../controller/sampling-controller");
 // router.get("/:id", UserController.getUser);
 router.get("/all/:date", SamplingController.getFirstIntents);
+router.get("/top5/:date", SamplingController.getTopFiveIntents);
 
 
 module.exports = router;
